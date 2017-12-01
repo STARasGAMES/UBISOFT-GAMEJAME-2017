c4bb1b8978d8b0d35cd9e0aa1634d55a6255b0dd
Some Sheat <doterpoter@outlook.com>
2017-12-02 01:29:15 +0200

    basic control

---
+++ New Unity Project/Assets/TarasTest/CatController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+
+    [SerializeField] Transform _pointer;
+    [SerializeField] float _force = 10;
+    [SerializeField] float _rotForce = 10;
+    [SerializeField] float _fromFloorForce = 10;
+    Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 dir = (_pointer.position - transform.position).normalized;
+        Vector3 force = (_pointer.position - transform.position).normalized * _force;
+        force = new Vector3(force.x, _fromFloorForce, force.z) * Time.fixedDeltaTime;
+        Debug.DrawRay(transform.position, force, Color.red, Time.fixedDeltaTime);
+        float angle = Vector3.SignedAngle(transform.forward, dir, Vector3.up);
+        if (Mathf.Abs(angle) > 10)
+            _rigidbody.AddTorque(Vector3.up * _rotForce * Mathf.Sign(angle));
+        if (Mathf.Abs(angle) < 90)
+            _rigidbody.AddForce(force, ForceMode.Acceleration);
+    }
+}
---
+++ New Unity Project/Assets/TarasTest/PointerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerController : MonoBehaviour {
+
+    [SerializeField] Transform _pointer;
+    [SerializeField] float _height = 0.1f;
+
+    Camera _camera;
+    Vector3 _input;
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        _input = Input.mousePosition;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        
+        RaycastHit hit;
+        Ray ray = _camera.ScreenPointToRay(_input);
+        if (Physics.Raycast(ray, out hit, 10000))
+        {
+            _pointer.position = hit.point + hit.normal.normalized * _height;
+            _pointer.rotation = Quaternion.LookRotation(hit.normal);
+            Debug.DrawRay(hit.point, hit.normal, Color.green, Time.fixedDeltaTime);
+        }
+	}
+}
