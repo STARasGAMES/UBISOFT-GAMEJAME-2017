f9be9966abf441b482f206f7479e04bb00fd03d6
therainycat <prineside@mail.ru>
2017-12-02 15:40:24 +0200

    Boss

---
+++ New Unity Project/Assets/TarasTest/BossHand.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHand : MonoBehaviour {
+	[SerializeField] Transform _boss;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		// TODO check if Cat was hit
+		_boss.GetComponent<BossController>()._currentAttackHit = true;
+	}
+}
