a2d1cdd982b091b7bd16d11db9eac79cf19f2389
therainycat <prineside@mail.ru>
2017-12-02 18:32:15 +0200

    Temp doggy

---
+++ New Unity Project/Assets/TarasTest/LootBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootBox : MonoBehaviour {
+	private GameObject _cat;
+
+	void Start () {
+		_cat = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject == _cat) {
+			Destroy (gameObject);
+
+			float r = Random.Range (0f, 3f);
+			if (r < 2f) {
+				// Weapon (66% probability)
+
+			} else {
+				// Fish (33%)
+
+			}
+		}
+	}
+}
