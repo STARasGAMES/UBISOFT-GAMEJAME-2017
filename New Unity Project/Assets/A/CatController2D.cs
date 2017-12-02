b032b58bd336854f5a5354182c150186660ff880
Some Sheat <doterpoter@outlook.com>
2017-12-02 14:23:55 +0200

    no message

--- New Unity Project/Assets/A/CatController2D.cs
+++ New Unity Project/Assets/A/CatController2D.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class CatController2D : MonoBehaviour {
 
-	[SerializeField] Transform _pointer;
-    [Header("Gravity")]
-    [SerializeField] float _gravity = 1;
-    [SerializeField] float _maxFallSpeed = 5;
-    [SerializeField] float _fallSpeed;
+	[SerializeField] PointerController _pointer;
+    [SerializeField] Transform _groundDetector;
     [Header("Moving")]
+    [SerializeField]
+    float _maxMoveSpeed = 5;
     [SerializeField] float _stopRange = 0.2f;
 	[SerializeField] float _visibilityRange = 5;
     [SerializeField] float _force = 10;
@@ -22,9 +21,9 @@ public class CatController2D : MonoBehaviour {
     {
         _rigidbody = GetComponent<Rigidbody2D>();
 
-		_climbObjects = GameObject.FindGameObjectsWithTag("Climb");
-		Debug.Log ("Climb objects");
-		Debug.Log (_climbObjects);
+		//_climbObjects = GameObject.FindGameObjectsWithTag("Climb");
+		//Debug.Log ("Climb objects");
+		//Debug.Log (_climbObjects);
     }
 
     // Use this for initialization
@@ -44,28 +43,68 @@ public class CatController2D : MonoBehaviour {
 
     private void FixedUpdate()
     {
-		// if (cat is on any of _climbObjects) {
-		//     disable gravity
-		//     allow cat to move on wall in direction of pointer if there's climb area 
-		//     
-		// } else {
-		//     enable physics
-		// }
+        // if (cat is on any of _climbObjects) {
+        //     disable gravity
+        //     allow cat to move on wall in direction of pointer if there's climb area 
+        //     
+        // } else {
+        //     enable physics
+        // }
         if (!IsGrounded())
         {
-            transform.position += Vector3.down * _gravity * Time.deltaTime;
+            Debug.Log("not Grounded");
+            return;
         }
 
-
-        Vector3 dir = _pointer.position - transform.position;
-        dir = new Vector3(dir.x, dir.y, 0);
-        if (dir.magnitude < _stopRange)
+        if (!CanSeePointer())
+        {
+            Debug.Log("dont see pointer");
             return;
-        Vector3 force = dir.normalized * _force;
-        force = new Vector3(force.x, _fromFloorForce, force.z) * Time.fixedDeltaTime;
-		Debug.DrawRay (transform.position, force, CanSeePointer () ? Color.green : Color.red, Time.fixedDeltaTime);
+        }
+        Vector2 targetPos = Vector3.zero;
 
-        float angle = Vector3.SignedAngle(transform.forward, dir, Vector3.up);
+        if (_pointer.isCastOnSomething)
+        {
+            if (_pointer.castedCollider.CompareTag("Obstacle"))
+            {
+                Debug.Log("Pointer on Obstacle");
+                return;
+            }
+            targetPos = _pointer.transform.position;
+        }
+        else
+        {
+            Vector2 pos = new Vector2(_pointer.transform.position.x, _pointer.transform.position.y);
+            Debug.DrawRay(pos, Vector2.down, Color.white);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.down, 100);
+            if (hit.transform == null)
+            {
+                Debug.Log("cant find collider");
+                return;
+            }
+            if (hit.collider.CompareTag("Obstacle"))
+            {
+                Debug.Log("Pointer on Obstacle");
+                return;
+            }
+            if (hit.collider.CompareTag("Walkable"))
+                targetPos = hit.point;
+            else
+                Debug.Log("error");
+
+        }
+        Vector2 some = targetPos - new Vector2(transform.position.x, transform.position.y);
+        Vector2 dir = new Vector2(some.x, some.y);
+        if (dir.magnitude < _stopRange)
+        {
+            Debug.Log("in stop range");
+            return;
+        }
+        Vector2 force = dir.normalized * _force * Time.fixedDeltaTime;
+        _rigidbody.AddForce(force);
+		Debug.DrawRay (transform.position, force, CanSeePointer () ? Color.green : Color.red, Time.fixedDeltaTime);
+        Debug.DrawRay(targetPos, Vector3.up, Color.yellow);
+        
         
 		// if (cat is moving up) {
 		// *     disable collisions (to climb from beneath)
@@ -84,10 +123,20 @@ public class CatController2D : MonoBehaviour {
 
     private bool IsGrounded()
     {
+        Vector2 pos = new Vector2(_groundDetector.position.x, _groundDetector.position.y);
+        var hit = Physics2D.Raycast(pos, Vector2.down, 0.2f);
+        Debug.DrawRay(transform.position, Vector3.down, Color.green);
+        if (hit.transform != null && (hit.point - pos).magnitude < _stopRange && hit.transform.CompareTag("Walkable"))
+            return true;
+        return false;
+    }
+
+    private bool CanMoveInDir(Vector2 dir)
+    {
         Vector2 pos = new Vector2(transform.position.x, transform.position.y);
-        var hit = Physics2D.Raycast(pos, Vector2.down, 0.1f);
-        
-        if (hit.transform != null && hit.transform.CompareTag("Walkable"))
+        var hit = Physics2D.Raycast(pos, dir, 1000);
+        Debug.DrawRay(transform.position, dir, Color.green);
+        if (hit.transform != null && (hit.point - pos).magnitude < _stopRange && hit.transform.CompareTag("Walkable"))
             return true;
         return false;
     }
